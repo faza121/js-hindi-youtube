4e27a91df641e3dc7ce5fc0ffe226353bdcc2ac4
Synthsol <56805417+faza121@users.noreply.github.com>
2025-07-03 14:59:08 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553;
+let accountEmail = "faza@google.com";
+var accountPassword = "12345"
+accountCity = "Faisalabad";
+
+
+//accountId = 2;//not allowed
+let accountState;
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+/*prefer not to use var because of issue in block cope and funtional scope*/
