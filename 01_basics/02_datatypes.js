550ef717ef58e71d6f01a5c0e07d60f0858fb4fc
Synthsol <56805417+faza121@users.noreply.github.com>
2025-07-03 16:15:42 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";// treat all JS code as newer verision/
+// alert("Hello"); we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("faza")
+
+let name = "faza"
+let age = 18
+let isLoggedIn = false
+
+Number
+BigInt
+string =>""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => unique 
+object 
